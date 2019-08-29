7e27848f60c499df10da90ae799385a0ccfbf571
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2019-08-29 09:31:03 +0700

    'add-all'

---
+++ app/src/main/java/com/lifetime/otherroomexample/MainActivity.java
@@ -0,0 +1,156 @@
+package com.lifetime.otherroomexample;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton buttonAddTask,buttonSortTask;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        buttonAddTask = findViewById(R.id.floating_button_add);
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSortTask = findViewById(R.id.floating_button_sort);
+        buttonSortTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sortTask();
+            }
+        });
+
+        getTasks();
+
+    }
+
+    private void sortTask(){
+        class SortTasks extends AsyncTask<Void,Void,List<Task>>{
+
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                List<Task> taskList = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .taskDao()
+                        .sortData();
+                return taskList;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                TaskAdapter adapter = new TaskAdapter(MainActivity.this,tasks);
+                recyclerView.setAdapter(adapter);
+//                Collections.sort(tasks);
+//                adapter.notifyDataSetChanged();
+            }
+        }
+
+        SortTasks gt = new SortTasks();
+        gt.execute();
+    }
+
+    private void getTasks(){
+        class GetTasks extends AsyncTask<Void,Void, List<Task>>{
+
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                List<Task> taskList = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .taskDao()
+                        .getAll();
+                return taskList;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                TaskAdapter adapter = new TaskAdapter(MainActivity.this,tasks);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+
+        GetTasks gt = new GetTasks();
+        gt.execute();
+
+    }
+
+    private void searchTasks(final String taskName){
+        class SearchTasks extends AsyncTask<Void,Void,List<Task>>{
+
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                List<Task> taskList = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .taskDao()
+                        .searchTaskByTaskName(taskName);
+                return taskList;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                TaskAdapter adapter = new TaskAdapter(MainActivity.this,tasks);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+
+        SearchTasks st = new SearchTasks();
+        st.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchTasks(s);
+                return false;
+            }
+        });
+        return true;
+    }
+}
---
+++ app/src/main/java/com/lifetime/otherroomexample/TaskAdapter.java
@@ -0,0 +1,80 @@
+package com.lifetime.otherroomexample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    private Context mCtx;
+    private List<Task> taskList;
+
+    public TaskAdapter(Context mCtx, List<Task> taskList) {
+        this.mCtx = mCtx;
+        this.taskList = taskList;
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mCtx).inflate(R.layout.recyclerview_tasks,parent,false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task task = taskList.get(position);
+        holder.bindView(task);
+        if(task.isFinished()){
+            holder.textViewStatus.setText("Completed");
+        }
+        else{
+            holder.textViewStatus.setText("Not Completed");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    public class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView textViewStatus, textViewTask, textViewDesc, textViewFinishBy;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+            textViewTask = itemView.findViewById(R.id.textViewTask);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
+            textViewFinishBy = itemView.findViewById(R.id.textViewFinishBy);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Task task = taskList.get(getAdapterPosition());
+
+            Intent intent = new Intent(mCtx,UpdateTaskActivity.class);
+            intent.putExtra("task",task);
+
+            mCtx.startActivity(intent);
+        }
+
+        public void bindView(Task task){
+            textViewTask.setText(task.getTask());
+            textViewDesc.setText(task.getDesc());
+            textViewFinishBy.setText(task.getFinishBy());
+        }
+    }
+}
